7dfac179dd129edcf2d5d3957becd8c5b185dd2f
Josh Justice <me@codingitwrong.com>
2020-08-21 08:30:29 -0400

    Add saving state

--- app/components/add-link-form.js
+++ app/components/add-link-form.js
@@ -7,13 +7,18 @@ export default class AddLinkFormComponent extends Component {
   @service store;
 
   @tracked url;
+  @tracked isSaving = false;
 
   @action
   async handleAdd(event) {
     event.preventDefault();
     let link = this.store.createRecord('bookmark', { url: this.url });
-    this.resetForm();
+
+    this.isSaving = true;
     await link.save();
+    this.isSaving = false;
+
+    this.resetForm();
 
     const { onAdd } = this.args;
     onAdd();
