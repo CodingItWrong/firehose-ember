4657dee5a8efa27608f14786b1e2d7a0df3c73a0
Josh Justice <me@codingitwrong.com>
2020-09-10 05:06:52 -0400

    Update login.js

--- app/routes/login.js
+++ app/routes/login.js
@@ -4,6 +4,7 @@ import { action } from '@ember/object';
 export default class LoginRoute extends Route {
   @action
   didTransition() {
+    // eslint-disable-next-line ember/no-controller-access-in-routes
     this.controller.resetLoginForm();
   }
 }
