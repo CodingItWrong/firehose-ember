c9889c8d6ed6e397620245f1ef081a05c5d8e705
Josh Justice <me@codingitwrong.com>
2020-09-24 05:55:47 -0400

    Unload tags to hide unused ones

--- app/routes/tags/index/data.js
+++ app/routes/tags/index/data.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 
 export default class TagIndexDataRoute extends Route {
   model() {
+    this.store.unloadAll();
     return this.store.findAll('tag', { reload: true });
   }
 }
