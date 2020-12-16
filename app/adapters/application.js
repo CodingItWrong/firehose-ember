6b6aaa85b9f324b36beca299d2f5cdf59a3663ee
Josh Justice <me@codingitwrong.com>
2020-12-16 08:02:05 -0500

    Convert app adapter to native class and remove computed

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,29 +1,22 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
 import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
 // eslint-disable-next-line ember/no-mixins
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import ENV from '../config/environment';
 
-let options = {
-  session: service('session'),
+export default class ApplicationAdapter extends JSONAPIAdapter.extend(
+  DataAdapterMixin,
+) {
+  @service session;
 
-  namespace: 'api',
+  host = ENV.apiHost;
+  namespace = 'api';
 
-  headers: computed(
-    'session.{isAuthenticated,data.authenticated.access_token}',
-    function () {
-      const headers = {};
-      if (this.session.isAuthenticated) {
-        headers.Authorization = `Bearer ${this.session.data.authenticated.access_token}`;
-      }
-      return headers;
-    },
-  ),
-};
-
-if (ENV.apiHost) {
-  options.host = ENV.apiHost;
+  get headers() {
+    const headers = {};
+    if (this.session.isAuthenticated) {
+      headers.Authorization = `Bearer ${this.session.data.authenticated.access_token}`;
+    }
+    return headers;
+  }
 }
-
-export default JSONAPIAdapter.extend(DataAdapterMixin, options);
