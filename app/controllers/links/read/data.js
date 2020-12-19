f1a4942b35c69c2a9b547c00fb63181b070d41ee
Josh Justice <me@codingitwrong.com>
2020-12-19 08:54:56 -0500

    Convert read controller to getters

--- app/controllers/links/read/data.js
+++ app/controllers/links/read/data.js
@@ -1,16 +1,18 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class ReadLinksController extends Controller {
-  pageNumber = 1;
-  searchText = '';
+  @tracked pageNumber = 1;
+  @tracked searchText = '';
+  @tracked editedSearchText = '';
 
   get totalPages() {
     return this.model.meta['page-count'];
   }
 
   reset() {
-    this.set('pageNumber', 1);
+    this.pageNumber = 1;
 
     // causes edited search text to flicker
     // this.set('searchText', '')
@@ -21,25 +23,25 @@ export default class ReadLinksController extends Controller {
   }
 
   setSearchTextFromQP() {
-    this.set('editedSearchText', this.searchText);
+    this.editedSearchText = this.searchText;
   }
 
   @action
   performSearch(e) {
     e.preventDefault();
-    this.set('searchText', this.editedSearchText);
-    this.set('pageNumber', 1);
+    this.searchText = this.editedSearchText;
+    this.pageNumber = 1;
   }
 
   @action
   nextPage() {
-    this.incrementProperty('pageNumber');
+    this.pageNumber += 1;
     this.scrollToTop();
   }
 
   @action
   prevPage() {
-    this.decrementProperty('pageNumber');
+    this.pageNumber -= 1;
     this.scrollToTop();
   }
 }
