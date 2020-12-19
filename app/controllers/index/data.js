bab1586de61c741f8fdfe2c7cc3d1bb5d13903e0
Josh Justice <me@codingitwrong.com>
2020-12-19 08:31:26 -0500

    Partially convert index controller to getters

--- app/controllers/index/data.js
+++ app/controllers/index/data.js
@@ -3,17 +3,19 @@ import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import { sort } from '@ember/object/computed';
 import { tracked } from '@glimmer/tracking';
+import sortBy from 'lodash/sortBy';
+import reverse from 'lodash/reverse';
 
 export default class IndexDataController extends Controller {
   @service session;
 
-  loggedInLinkSorting = Object.freeze(['moved_to_list_at:desc']);
   loggedOutLinkSorting = Object.freeze(['published_at:desc']);
 
   @tracked pageNumber = 1;
 
-  @sort('model', 'loggedInLinkSorting')
-  loggedInSortedLinks;
+  get loggedInSortedLinks() {
+    return reverse(sortBy(this.model.toArray(), ['moved_to_list_at']));
+  }
 
   @sort('model', 'loggedOutLinkSorting')
   loggedOutSortedLinks;
