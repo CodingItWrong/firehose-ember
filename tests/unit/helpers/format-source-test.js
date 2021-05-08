36e51796a46b41130be378c09393ae318c46a78c
Josh Justice <me@codingitwrong.com>
2021-05-08 10:05:00 -0400

    Switch to QUnit

--- tests/unit/helpers/format-source-test.js
+++ tests/unit/helpers/format-source-test.js
@@ -1,16 +1,15 @@
+import { module as describe, test as it } from 'qunit';
 import { formatSource } from 'firehose/helpers/format-source';
-import { describe, it } from 'mocha';
-import { expect } from 'chai';
 
-describe('formatSource()', () => {
-  it('formats URLs as a link', () => {
+describe('Unit | Helper | formatSource', () => {
+  it('formats URLs as a link', function (assert) {
     let sourceURL = 'https://example.com/page';
     let result = formatSource(sourceURL);
-    expect(result).to.eq(`<a href="${sourceURL}">example.com</a>`);
+    assert.deepEqual(result, `<a href="${sourceURL}">example.com</a>`);
   });
 
-  it('returns non-URLs as-is', () => {
+  it('returns non-URLs as-is', function (assert) {
     const nonURLSource = 'a friend';
-    expect(formatSource(nonURLSource)).to.eq(nonURLSource);
+    assert.deepEqual(formatSource(nonURLSource), nonURLSource);
   });
 });
