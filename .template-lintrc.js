2e0eac1c70fce5f0a6330766f298f462efb2e5e4
Josh Justice <me@codingitwrong.com>
2021-11-06 13:20:04 -0400

    Update Ember to 3.28 and fix Node to <= 14

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  extends: ['octane', 'stylistic'],
+  extends: ['recommended', 'stylistic'],
   rules: {
     'eol-last': 'always',
     'require-input-label': 'off',
